90f85b806169699f7b2824af023f26d24c775691
Kotamoss <storyofko@gmail.com>
2018-04-10 12:07:05 -0400

    built the homepage UI and BorderButton Class

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by R$KY on 4/10/18.
+//  Copyright © 2018 Dakotah Moss. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
